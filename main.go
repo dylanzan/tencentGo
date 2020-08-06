c07f0c8da54adec055943950683fd8250a8cb565
dylan <zsc9602@163.com>
2020-08-06 18:20:41 +0800

    fv

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/spf13/viper"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
-	"net/http/httputil"
+	fastHttp "github.com/valyala/fasthttp"
+	proxy "github.com/yeqown/fasthttp-reverse-proxy"
+	//"net/http/httputil"
 	"net/url"
-	"strconv"
 	"strings"
 	"sync"
 	pb_tencent "tencentgo/model/tencent"
@@ -74,49 +74,77 @@ func contains(s []string, e string, isExact bool) bool {
 	return false
 }
 
-func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+func (t transport)RoundTrip(ctx *fastHttp.RequestCtx) (ctxResp *fastHttp.RequestCtx) {
 	// copy request
-	b, err := ioutil.ReadAll(req.Body)
-
+	//b, err := ioutil.ReadAll(req.Body)
+	b:=ctx.Request.Body()
 	//process request change
 	b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1)
 	newRequest := &pb_tencent.Request{}
-	err = proto.Unmarshal(b, newRequest)
+	err := proto.Unmarshal(b, newRequest)
+
+	if err!=nil{
+		return nil
+	}
 
 	//modify b if necessary
+	ctx.Request.SetBody(b)
 
+	req:=fastHttp.AcquireRequest()
+	resp:=fastHttp.AcquireResponse()
+
+	defer func() {
+		fastHttp.ReleaseResponse(resp)
+		fastHttp.ReleaseRequest(req)
+	}()
+
+	req.Header.SetContentLength(len(b))
+	req.Header.SetMethod("POST")
+	req.SetRequestURI(string(ctx.Request.Header.Host()))
+
+
+
+	req.SetBody(b)
+
+	t.RoundTripper.RoundTrip(req)
+
+	/*if err:=fastHttp.Do(req,resp);err!=nil{
+		log.Println("request err",err.Error())
+		return
+	}*/
+
+	b=resp.Body()
 	//turn to pb and set back
-	data, err := proto.Marshal(newRequest) //TODO: if no changed, just send original pb to http
-	body := ioutil.NopCloser(bytes.NewReader(data))
-	req.Body = body
-	req.ContentLength = int64(len(data))
-	req.Header.Set("Content-Length", strconv.Itoa(len(data)))
+	//data, err := proto.Marshal(newRequest) //TODO: if no changed, just send original pb to http
+	//body := ioutil.NopCloser(bytes.NewReader(data))
+	//ctx.SetBody(b)
+	//ctx.Request.Header.Set("Content-Length",strconv.Itoa(len(data)))
+	//req.Body = body
+	//req.ContentLength = int64(len(data))
+	//req.Header.Set("Content-Length", strconv.Itoa(len(data)))
 
 	//set back
 	//req.Body = ioutil.NopCloser(bytes.NewBuffer(b))
 
 	// reverse proxy
-	resp, err = t.RoundTripper.RoundTrip(req)
+	//resp, err = t.RoundTripper.RoundTrip(ctx.Request)
 
 	// error to nil return
+/*	err = req.Body.Close()
 	if err != nil {
 		return nil, err
-	}
-	err = req.Body.Close()
-	if err != nil {
-		return nil, err
-	}
+	}*/
 
 	//TODO: should be error return here, to find out a new solution
 
-	b, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
+	//b, err = ioutil.ReadAll(resp.Body)
+	/*if err != nil {
 		return nil, err
-	}
-	err = resp.Body.Close()
-	if err != nil {
+	}*/
+	//err = resp.Body.Close()
+/*	if err != nil {
 		return nil, err
-	}
+	}*/
 	b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1)
 	//body = ioutil.NopCloser(bytes.NewReader(b))
 	newResponse := &pb_tencent.Response{}
@@ -140,26 +168,30 @@ func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error
 	fmt.Println("roundTrip RESPRESPRESPRESP  " + newResponse.String())
 
 	// pb object to response body and return to hhtp
-	data, err = proto.Marshal(newResponse) //TODO: if no changed, just send original pb to http
-	body = ioutil.NopCloser(bytes.NewReader(data))
-	resp.Body = body
-	resp.ContentLength = int64(len(data))
-	resp.Header.Set("Content-Length", strconv.Itoa(len(data)))
-	return resp, nil
+	data, err := proto.Marshal(newResponse) //TODO: if no changed, just send original pb to http
+	ctxResp.Response.SetBody(data)
+	ctxResp.Response.Header.SetContentLength(len(data))
+	//resp.Header.Set("Content-Length", strconv.Itoa(len(data)))
+	//ctxResp.Response.
+	return ctxResp
 }
 
-func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (this *handle) ServeHTTP(ctx *fastHttp.RequestCtx) {
 
-	b, err := ioutil.ReadAll(r.Body)
+	//b, err := ioutil.ReadAll(r.Body)
+	b:=ctx.Request.Body()
 
-	if err != nil {
-		log.Printf("read all err is %v", err)
-	}
 
 	//process request change
 	b = bytes.Replace(b, []byte("server"), []byte("schmerver"), -1)
 	newRequest := &pb_tencent.Request{}
-	err = proto.Unmarshal(b, newRequest)
+	err := proto.Unmarshal(b, newRequest)
+
+	if err!=nil{
+		log.Println("proto parse err is :",err)
+		return
+	}
+
 	//res, _ := json.Marshal(newRequest)
 	//log.Println(string(res))
 	addr := rconfig.DefaultUpstreamAddr
@@ -229,11 +261,11 @@ func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				}
 				data, err := proto.Marshal(newResponse) //TODO: if no changed, just send original pb to http
 				if err != nil {
-					w.WriteHeader(204)
+					ctx.Response.SetStatusCode(204)
+					//w.WriteHeader(204)
 				}
+				ctx.Write(data)
 				//bodyMap[*(newRequest.Impression[0].Dealid)] = bodyContent{bodycontent.body, bodycontent.cnt + 1}
-				w.Write(data)
-
 				//fmt.Println("serverHttp")
 				fmt.Println("serverHttp REQREQREQREQ         " + newRequest.String())
 				fmt.Println("serverHttp RESPRESPRESPRESP     " + newResponse.String())
@@ -242,11 +274,19 @@ func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 
 		//if not in bodyMap, reverseProxy and transpot RoundTrip,
-		body := ioutil.NopCloser(bytes.NewReader(b))
-		r.Body = body
-		proxy := httputil.NewSingleHostReverseProxy(remote)
-		proxy.Transport = &transport{http.DefaultTransport}
-		proxy.ServeHTTP(w, r)
+		//body := ioutil.NopCloser(bytes.NewReader(b))
+		//r.Body = body
+
+		ctx.SetBody(b)
+		ctx.Request.Header.Set("Host",remote.String())
+		proxyServer:=proxy.NewReverseProxy(remote.String())
+		//proxyServer.
+		proxyServer.ServeHTTP(RoundTrip(remote.String(),ctx))
+
+		//proxy := httputil.NewSingleHostReverseProxy(remote)
+		//proxy.Transport = &transport{http.DefaultTransport}
+		//proxy.ServeHTTP(w, r)
+
 
 	}
 
@@ -256,18 +296,20 @@ func startServer() {
 	//被代理的服务器host和port
 	h := &handle{}
 
-	srv := http.Server{
+	/*srv := http.Server{
 		Addr:    ":" + rconfig.ListenPort,
 		Handler: h,
 		//ReadTimeout:  20 * time.Second,
 		//WriteTimeout: 20 * time.Second,
-	}
+	}*/
 
-	fmt.Println(srv.Addr)
-	err := srv.ListenAndServe()
+	//fmt.Println(srv.Addr)
+	//err := srv.ListenAndServe()
+
+	err:=fastHttp.ListenAndServe(":"+rconfig.ListenPort,h.ServeHTTP)
 
 	if err != nil {
-		log.Fatalln("ListenAndServe: ", err)
+		log.Fatalln("start http err is : ", err)
 	}
 }
 
